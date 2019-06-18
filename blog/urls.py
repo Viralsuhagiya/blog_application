1bcad9baf7c376c8736314b6c9897b1bbc3db826
Viralsuhagiya <viralsuhagiya02@gmail.com>
2019-06-18 14:06:52 +0530

    updated setting

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from .views import (PostListView,PostDetailView,PostCreateView,PostUpdateView, P
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('blog/', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
