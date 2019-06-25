b295359caaac11b20e4a6018f1f8d05bea535bbc
viralsuhagiya02@gmail.com <viralsuhagiya02@gmail.com>
2019-06-25 15:33:14 +0530

    [UPDATE] about and home page

--- blog_application/settings.py
+++ blog_application/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY="os.environ.get('SECRET_KEY')"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mydjangoblogapplication.herokuapp.com']
+ALLOWED_HOSTS = ['myawesomecart.herokuapp.com']
 
 # Application definition
 
